37287c24725ffa1cf89e6237350598f77f1dc176
sujan-ynwa <38416311+sujan-ynwa@users.noreply.github.com>
2020-03-27 14:03:39 +0545

    @componentScan added on main class

--- src/main/java/com/summerproject/cctvnepal/CctvnepalApplication.java
+++ src/main/java/com/summerproject/cctvnepal/CctvnepalApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan(basePackages={"com.summerproject"})
 public class CctvnepalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CctvnepalApplication.class, args);
